0d9780fca27e70dfea844f69ee47bf6471e5977c
Patrick Steele-Idem <pnidem@gmail.com>
2014-03-15 16:29:07 -0600

    Initial commit

---
+++ raptor-optimizer-minify-css.js
@@ -0,0 +1,33 @@
+var sqwish = require('sqwish');
+
+function minify(src, options) {
+    if (!options) {
+        options = {};
+    }
+    
+    //var strict = options.mergeDuplicates !== false;
+    return sqwish.minify(src, false);
+}
+
+module.exports = {
+    contentType: 'css',
+    
+    name: module.id,
+
+    stream: false,    
+
+    transform: function(code, contentType, dependency, bundle) {
+        if (contentType === 'css') {
+            var mergeDuplicates = dependency.mergeDuplicates !== false;
+
+            var minified = minify(code, {
+                mergeDuplicates: mergeDuplicates
+            });
+            
+            return minified;
+        }
+        else {
+            return code;
+        }
+    }
+};
